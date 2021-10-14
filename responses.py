b915f9e2d0218b0da18a572cd7d3b368ceb3c9f3
Miguel Elkjaer Gastelum Ahumada <89278969+ElMe6a@users.noreply.github.com>
2021-10-14 12:25:51 -0600

    [UPDATE] se agregaron mas respuestas

--- responses.py
+++ responses.py
@@ -19,11 +19,12 @@ def process_message(message, response_array, response):
 
 def get_response(message):
     # Agregar las respuestas Array
-    Daniel=["No man!","Quien es tu perra?", "Vete alv me gano la casa", "No le hago a eso", "yo no culeo gatods", "No me dormi man", "Zzzzz...."]
+    
     Rene=["Espera estoy midiendo semillas...", "Apenas llevo 2300 semillas", "Vete por ellas!", "Sho solo soy el practicante!","Quien quiere jugar cartitas?"]
     Daniel=["Aaron me das un besito?","No man!","Quien es tu perra?", "Vete alv me gano la casa", "No le hago a eso", "yo no culeo gatos", "No me dormi", "Zzzzz...."]
     David=["Necesito un buen rifle","Necesito el grande, el chico no me llena (escudo)","Quiero ver gotas", "Ya me enbetune el pantalón"]
-    Daniel=["este man","No man!","Quien es tu perra?", "Vete alv me gano la casa", "No le hago a eso", "yo no culeo gatos", "No me dormi", "Zzzzz...."]
+    Daniel=["este man","Alguien sabe si la doctora ya le hizo examen de prostata?","No man!","Quien es tu perra?", "Vete alv me gano la casa", "No le hago a eso", "yo no culeo gatos", "No me dormi man", "Zzzzz...."]
+    #Daniel=["Alguien sabe si la doctora ya le hizo examen de prostata?"]
     Shecho=["Ya weey!!!", "Estas bien pendejo Daniel", "Su ptm ya me quemaron", "Sacalas VL!", "Me trague mi propio pedo alv"]
     VL=["Estoy tomando", "Pssst (ruido de cheve)","Se me chingo la tele", "Deje cansado al shecho"]
     Aaron=["Ya me culie al Daniel","Daniel a mi me dijeron que culeas gatos!", "Me acabo de fumar un gallito"]
@@ -31,7 +32,7 @@ def get_response(message):
     Line=["Como chinga oiga", "Aqui no vive", "Miauuu... a te la creiste", "Para que le hablas si eres alergico pinche Dani prro"]
     Mega=["Lo deje a un tiro", "Pinche daniel esta mas turbado que nunca"]
     risa=["De que te ries...!", "Si mucha risa!"]
-    Juego=["Esta!!", "Si mucha risa!"]
+    Juego=["Esta!!", "No le digan al Daniel de todos modos muere rapido"]
     Caracolaa=["Si", "No", "Tal vez", "Siempre", "Probablemente", "Espera...", "No puedo contestar, me dio lag mental", "Si"]
     response_list = [
         
@@ -49,7 +50,7 @@ def get_response(message):
         process_message(message, ['No man no man no man','no man no man no man'], 'Puuum... Leeroy Jenkins!!!! jajajaja no te creas man... ya llegue alguien me hablo?'),
         process_message(message, ['lag'], 'Alguien ha visto al Daniel?'),
         process_message(message, ['prestame', 'Prestame'], 'Esta!!!'),
-        process_message(message, ['https://www.facebook.com' + 'r"[\s]'], 'Jajajaja ya lo vi!!'),
+        process_message(message, ['https://www.facebook.com/'], 'Jajajaja ya lo vi!!'),
         process_message(message, ['puto bot', 'Puto bot'], 'Puto tu... quieres que publique tus fotos puteando?'),
         process_message(message, ['jajaja', 'jajajaja','jajajajaja', 'jajajjajaja'], random.choice(risa)),
         process_message(message, ['bocina'], 'La pinche bocina es mia y mis huevotes!'),
@@ -58,13 +59,14 @@ def get_response(message):
         process_message(message, ['Lineee', 'lineee', 'paquira'], random.choice(Line)),
         process_message(message, ['no man', 'No man'], 'Alguien a visto al Danielll?'),
         process_message(message, ['oxxo', 'Oxxo'], 'Alguien a visto al Rene?'),
+        process_message(message, ['chiquito', 'Chiquito'], 'Prestas!'),
         process_message(message, ['cumpleañero', 'Cumpleañero'], 'Alguien se va a sentar a comer el pastel!'),
         process_message(message, ['hora?','Hora?', 'que horas son?','Que hora son?'], datetime.now().strftime('%H:%M %p')),
         process_message(message, ['que dia es hoy?','Que dia es hoy?', 'a que estamos?','A que estamos?'], datetime.now().strftime('%d/%m/%Y')),
         process_message(message, ['cual saco?','Cual saco?','que saco?', 'Que saco?'], 'Esta!!'),
         process_message(message, ['cual recibo?','Cual recibo?','que recibo?', 'Que recibo?'], 'Esta!!'),
         process_message(message, ['chupo', 'Chupo'], 'Esta!!'),
-        process_message(message, ['saco', 'Saco', 'cual saco?', 'Cual saco?', 'Cuál saco?'], 'Esta!!'),
+        process_message(message, ['saco', 'Saco', 'cual saco?', 'cúal saco?', 'Cual saco?', 'Cuál saco?'], 'Esta!!'),
         process_message(message, ['de ti puto boy', 'De ti puto boy'], 'Buscate otro payaso!!'),
         process_message(message, ['chinga tu madre', 'Chinga tu madre'], 'la tuya en vinagre!!'),
         process_message(message, ['pinche bot', 'Pinche bot','pinche Bot', 'Pinche Bot', 'Pinchi Bot', 'Pinchi Bot'], 'La tuya pendeja!!'),
